5b01ea41cadb8f243e63ba5b97c1477983fd9d99
Ronit <ronitkhatri44@gmail.com>
2022-10-10 12:02:24 +0530

    updating progress

--- Dynamic Programming/fibonacci-memoization.cpp
+++ Dynamic Programming/fibonacci-memoization.cpp
@@ -3,8 +3,23 @@
 
 using namespace std;
 
+// TC: O(n)
+// SC: O(2n)
+// Memoization (top-down approach)
+int f(int n, vector<int> &dp){
+    if(n <= 1) return n;
+
+    if(dp[n] != -1) return dp[n];
+    return dp[n] = f(n-1,dp) + f(n-2,dp);
+}
+
 int main(){
 
+    int n=5;
+
+    // fibonacci (0 1 1 2 3 5 8 ...)
+    vector<int> dp(n+1, -1);
+    cout << f(n, dp);
 
     return 0;
 }
