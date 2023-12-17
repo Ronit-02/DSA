0e55115b40fcdd6d2b0d0b63d7aed0e202ecd78f
Ronit <ronitkhatri44@gmail.com>
2023-12-17 13:51:33 +0530

    updating content

--- 2D Array/spiral-print.cpp
+++ 2D Array/spiral-print.cpp
@@ -1,54 +1,54 @@
-
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void spiralPrint(int arr[][4], int r, int c){
+vector<int> spiralPrint(vector<vector<int>> mat, int r, int c){
 
-    //4 variables req
-    int startRow = 0;
-    int endRow = r-1;
-    int startCol = 0;
-    int endCol = c-1;
+    // row
+    int left = 0;
+    int right = c-1;
+    // col
+    int top = 0;
+    int bottom = r-1;
 
-    while(startCol <= endCol && startRow <=endRow){
+    vector<int> ans;
 
-        //Start Row
-        for(int col = startCol; col<=endCol; col++)
-            cout<< arr[startRow][col] <<" ";
+    while(top <= bottom && left <= right){
 
-        //End Col
-        for(int row = startRow+1; row<=endRow; row++)
-            cout<< arr[row][endCol] <<" ";
-        
-        //End Row
-        for(int col = endCol-1; col>=startCol; col--){
-            //to avoid duplicate printing of rows when cols are less
-            if(startRow==endRow)
-                break;
-            cout<< arr[endRow][col] <<" ";
-            
+        // left to right
+        for(int i=left; i<=right; i++){
+            ans.push_back(mat[top][i]);
+        }
+        top++;
+
+        // top to bottom
+        for(int i=top; i<=bottom; i++){
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+
+        // right to left
+        if(top <= bottom){
+            for(int i=right; i>=left; i--){
+                ans.push_back(mat[bottom][i]);
+            }
+            bottom--;
         }
 
-        //Start Col
-        for(int row = endRow-1; row>=startRow+1; row--){
-
-            //to avoid duplicate printing of rows when cols are less
-            if(startCol==endCol)
-                break;
-            cout<< arr[row][startCol] <<" ";
+        // bottom to top
+        if(left <= right){
+            for(int i=bottom; i>=top; i--){
+                ans.push_back(mat[i][left]);
+            }
+            left++;
         }
-
-        // Update the variables to point to inner spiral
-        startRow++;
-        endRow--;
-        startCol++;
-        endCol--;
     }
+    return ans;
 }
 
 int main() {
 
-    int arr[][4] = {
+    vector<vector<int>> mat = {
         {1,2,3,4},
         {5,6,7,8},
         {9,10,11,12},
@@ -57,16 +57,9 @@ int main() {
     int r = 4;
     int c = 4;
 
-    cout<<"Array: \n";
-    for(int i=0; i<r; i++){
-        for(int j=0; j<c; j++){
-            cout<<arr[i][j] <<" ";
-        }
-        cout<<endl;
-    }
-
-    cout<<"\nSpiral Print: \n";
-    spiralPrint(arr, r, c);
+    vector<int> res = spiralPrint(mat, r, c);
+    for(auto it: res)
+        cout << it << " ";
 
 	return 0;
 }
