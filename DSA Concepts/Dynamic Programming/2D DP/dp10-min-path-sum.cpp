db6412bd85aeef6609c829e1256fe6c13885f93e
Ronit Khatri <ronitkhatri44@gmail.com>
2024-01-03 21:18:10 +0530

    practicing 2d dp

--- DSA Concepts/Dynamic Programming/2D DP/dp10-min-path-sum.cpp
+++ DSA Concepts/Dynamic Programming/2D DP/dp10-min-path-sum.cpp
@@ -3,38 +3,113 @@
 #include <algorithm>
 using namespace std;
 
-// Question:
+// Question: Find minimum path sum from top-left to right-bottom,
+// given that you can only move down and right
 
 // Recursion (req value to base case)
-int f1(vector<int> &nums)
+// Min path sum from 0,0 -> i,j
+int f1(int i, int j, vector<vector<int>> &grid)
 {
+    if(i==0 && j==0)
+        return grid[0][0];
+    if(i<0 || j<0)
+        return INT_MAX;
+
+    int up = f1(i-1, j, grid);
+    int left = f1(i, j-1, grid);
+
+    return grid[i][j] + min(up, left);
 }
 
 // Memoization
-int f2()
+int f2(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &dp)
 {
+    if(i==0 && j==0)
+        return grid[0][0];
+    if(i<0 || j<0)
+        return INT_MAX;
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    int up = f2(i-1, j, grid, dp);
+    int left = f2(i, j-1, grid, dp);
+
+    return dp[i][j] = grid[i][j] + min(up, left);
 }
 
 // Tabulation
-int f3()
+int f3(vector<vector<int>> &grid)
 {
+    int m = grid.size();
+    int n = grid[0].size();
+    vector<vector<int>> dp(m, vector<int> (n, 0));
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0)
+                dp[i][j] = grid[i][j];
+            else{
+                int up = INT_MAX;
+                int left = INT_MAX;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+
+                dp[i][j] = grid[i][j] + min(up, left);
+            }
+        }
+    }
+
+    return dp[m-1][n-1];
 }
 
 // Space Optimization
-int f4()
+int f4(vector<vector<int>> &grid)
 {
+    int m = grid.size();
+    int n = grid[0].size();
+
+    vector<int> prev(n, INT_MAX);
+
+    for(int i=0; i<m; i++){
+        vector<int> cur(n, 0);
+        
+        for(int j=0; j<n; j++){
+            if(i==0 && j==0)
+                cur[j] = grid[i][j];
+            else{
+                int up = INT_MAX;
+                int left = INT_MAX;
+                up = prev[j];
+                if(j>0) left = cur[j-1];
+
+                cur[j] = grid[i][j] + min(up, left);
+            }
+        }
+
+        prev = cur;
+    }
+
+    return prev[n-1];
 }
 
-int maxNonAdjSum(vector<int> &nums)
+int minPathSum(vector<vector<int>> &grid)
 {
-    // vector<int> dp(n + 1, -1);
-    return f1(nums);
+    int m = grid.size();
+    int n = grid[0].size();
+
+    // return f1(m-1, n-1, grid);
+
+    // vector<vector<int>> dp(m, vector<int> (n, -1));
+    // return f2(m-1, n-1, grid, dp);
+
+    // return f3(grid);
+    return f4(grid);
 }
 
 int main()
 {
-    vector<int> nums = {1, 2, 4};
-    cout << maxNonAdjSum(nums);
+    vector<vector<int>> grid = {{1,3,1}, {1,5,1}, {4,2,1}};
+    cout << minPathSum(grid);
 
     return 0;
 }
--- DSA Concepts/Dynamic Programming/2D DP/dp11-triangle.cpp
+++ DSA Concepts/Dynamic Programming/2D DP/dp11-triangle.cpp
@@ -3,21 +3,48 @@
 #include <algorithm>
 using namespace std;
 
-// Question:
+// Question: Find min path sum from top to bottom of triangle,
+// given that you can only move from i -> i, i+2
 
 // Recursion (req value to base case)
-int f1(vector<int> &nums)
+// Min path sum from i,j to n-1
+int f1(int i, int j, vector<vector<int>>& triangle)
 {
+    if(i == triangle.size()-1)
+        return triangle[i][j];
+    
+    int down = triangle[i][j] + f1(i+1, j, triangle);
+    int downRight = triangle[i][j] + f1(i+1, j+1, triangle);
+
+    return min(down, downRight);
 }
 
 // Memoization
-int f2()
+int f2(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp)
 {
+    if(i == triangle.size()-1)
+        return triangle[i][j];
+    if(dp[i][j] != -1)
+        return dp[i][j];
+    
+    int down = triangle[i][j] + f2(i+1, j, triangle, dp);
+    int downRight = triangle[i][j] + f2(i+1, j+1, triangle, dp);
+
+    return dp[i][j] = min(down, downRight);
 }
 
 // Tabulation
-int f3()
+int f3(vector<vector<int>> &triangle)
 {
+    int n = triangle.size();
+    vector<vector<int>> dp(n, vector<int> (n, 0));
+
+    for(int i=0; i<n; i++)
+        dp[n-1][i] = triangle[n-1][i];
+    
+    for(int i=n-2; i>=0; i--){
+        dp[]
+    }
 }
 
 // Space Optimization
@@ -25,16 +52,23 @@ int f4()
 {
 }
 
-int maxNonAdjSum(vector<int> &nums)
+int Triangle(vector<vector<int>> &triangle)
 {
-    // vector<int> dp(n + 1, -1);
-    return f1(nums);
+    int n = triangle.size();
+
+    // return f1(0, 0, triangle);
+
+    // vector<vector<int>> dp(n, vector<int> (n, -1));
+    // return f2(0, 0, triangle, dp);
+
+    return f3(triangle);
+    // return f4(triangle);
 }
 
 int main()
 {
-    vector<int> nums = {1, 2, 4};
-    cout << maxNonAdjSum(nums);
+    vector<vector<int>> triangle = {{2}, {3,4}, {6,5,7}, {4,1,8,3}};  // 11
+    cout << Triangle(triangle);
 
     return 0;
 }
