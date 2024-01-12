dfe4659e83fa44238d05d0e10ffc5f8cc46cf719
Ronit Khatri <ronitkhatri44@gmail.com>
2024-01-12 12:21:15 +0530

    updating dp on strings

--- Algorithms/Dynamic Programming/String DP/6-distinct-subseq.cpp
+++ Algorithms/Dynamic Programming/String DP/6-distinct-subseq.cpp
@@ -6,18 +6,58 @@ using namespace std;
 // Question:
 
 // Recursion (req value to base case)
-int f1(vector<int> &nums)
-{
+int f1(int i, int j, string &s, string &t){
+
+    if(j < 0)
+        return 1;
+    if(i < 0) 
+        return 0;
+
+    if(s[i] == t[j])
+        return f1(i-1, j-1, s, t) + f1(i-1, j, s, t);
+    
+    return f1(i-1, j, s, t);
 }
 
 // Memoization
-int f2()
-{
+int f2(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+
+    if(j < 0)
+        return 1;
+    if(i < 0) 
+        return 0;
+    if(dp[i][j] != -1)
+        return dp[i][j];
+
+    if(s[i] == t[j])
+        return dp[i][j] = f2(i-1, j-1, s, t, dp) + f2(i-1, j, s, t, dp);
+    
+    return dp[i][j] = f2(i-1, j, s, t,dp);
 }
 
 // Tabulation
-int f3()
+int f3(string &s, string &t)
 {
+    int m = s.size();
+    int n = t.size();
+    int mod = 1e9+7;
+
+    vector<vector<double>> dp(m+1, vector<double> (n+1, 0)); 
+
+    for(int i=0; i<=m; i++) dp[i][0] = 1;
+    for(int j=1; j<=n; j++) dp[0][j] = 0;
+
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            
+            if(s[i-1] == t[j-1])
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            else 
+                dp[i][j] = dp[i-1][j];
+        }
+    }
+
+    return (int)dp[m][n];
 }
 
 // Space Optimization
@@ -27,7 +67,15 @@ int f4()
 
 int distinctSubsequences(string &s1, string &s2)
 {
-    return f1(s1, s2);
+    // int m = s1.size();
+    // int n = s2.size();
+
+    // return f1(m - 1, n - 1, s1, s2);
+
+    // vector<vector<int>> dp(m, vector<int>(n, -1));
+    // return f2(m - 1, n - 1, s, t, dp);
+
+    return f3(s1, s2);
 }
 
 int main()
--- Algorithms/Dynamic Programming/String DP/7-edit-distance.cpp
+++ Algorithms/Dynamic Programming/String DP/7-edit-distance.cpp
@@ -6,8 +6,20 @@ using namespace std;
 // Question:
 
 // Recursion (req value to base case)
-int f1(vector<int> &nums)
-{
+int f1(int i, int j, string &s, string &t){
+
+    if(i < 0) return j+1;
+    if(j < 0) return i+1;
+
+    if(s[i] == t[j])
+        return f1(i-1, j-1, s, t);
+    
+    // insert, delete, replace
+    return 1 + min({
+        f1(i, j-1, s, t), 
+        f1(i-1, j, s, t), 
+        f1(i-1, j-1, s, t)
+    });
 }
 
 // Memoization
@@ -16,8 +28,32 @@ int f2()
 }
 
 // Tabulation
-int f3()
+int f3(string word1, string word2)
 {
+    int m = word1.size();
+    int n = word2.size();
+    vector<vector<int>> dp(m+1, vector<int> (n+1, 0));
+
+    for(int j=0; j<=n; j++) dp[0][j] = j;
+    for(int i=0; i<=m; i++) dp[i][0] = i;
+
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+
+            if(word1[i-1] == word2[j-1]){
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else{
+                dp[i][j] = 1 + min({
+                    dp[i][j-1],
+                    dp[i-1][j],
+                    dp[i-1][j-1]
+                });
+            }
+        }
+    }
+
+    return dp[m][n];
 }
 
 // Space Optimization
@@ -25,9 +61,12 @@ int f4()
 {
 }
 
-int editDistance(string &s1, string &s2)
+int editDistance(string &word1, string &word2)
 {
-    return f1(s1, s2);
+    int m = word1.size();
+    int n = word2.size();
+
+    return f1(m-1, n-1, word1, word2);
 }
 
 int main()
